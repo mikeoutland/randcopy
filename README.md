c8604a340e08fee8b9aa902a527364aa9f8a3167
mikeoutland-nexusis <mike.outland@nexusis.com>
2015-04-23 14:09:36 -0700

    Major updates to randcopy.
    Now shows status and ETA of the copy
    
    Now supports conversion of .flac and .wma to .mp3 by using ffmpeg.
    
    Checks for existence of ffmpeg
    
    Added help section

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# randcopy
+	Randcopy was created by Mike Outland at https://github.com/mikeoutland-nexusis/randcopy
 
-Randcopy is a python utility to randomly copy music from one directory to another.
+	Randcopy is a utility to randomly copy music from a large local datastore to another local datastore.
+	A typical use case might be to generate a random collection of music on a USB thumb drive.
 
-This tool is useful when you have a large store of music and wish to copy a set of random music files to a usb drive.
+	Randcopy requires ffmpeg to be installed on the system allowing conversion from .flac and .wma to .mp3 (@320kbps).
+
+	Randcopy takes 3 parameters:
+		1.  Source directory:
+			This is a source directory that must be visible from the command line where music to be copied is located. Randcopy will automatically traverse sub directories.
+		2.  Destination directory:
+			This is a directory that must be visible from the command line where muisc will be copied and converted to. All files will retain their ID3 tags but will NOT retain source directory structure.
+		3.  Number of files to copy:
+			This is the number of files you wish  to copy or convert from the source to the destination.
--- randcopy.py
+++ randcopy.py
@@ -6,9 +6,12 @@ import os
 import errno
 import fnmatch
 import subprocess
+import math
+import datetime
 from random import randint
 
-supportedExtensions = ('.mp3')
+
+supportedExtensions = ('.mp3','.flac','.wma')
 
 def populateFileList(srcPath, extensions):
 	allFiles = []
@@ -18,34 +21,78 @@ def populateFileList(srcPath, extensions):
 				allFiles.append(os.path.join(root, f))
 	return allFiles
 
-def copyMp3(srcPath, dstPath):
+def copyOrConvert(srcPath, dstPath):
 	filePath, fileExtension = os.path.splitext(srcPath)
 	fileName = os.path.basename(filePath)
-	shutil.copy2(srcPath, dstPath)
+	try:
+		if fileExtension == '.mp3':
+			shutil.copy2(srcPath, dstPath)
+		else:
+			subprocess.call(["ffmpeg", "-loglevel", "panic", "-nostats", "-y", "-i", srcPath, "-ab",  "320k",  "-map_metadata",  "0",  "-id3v2_version", "3", (dstPath + "/" + fileName + ".mp3")])
+	except:
+		print("Exception, moving along..")
 
 def randomCopy(srcPathList, dstPath, fileCount):
+	startTime = datetime.datetime.now()
 	for x in range(0, fileCount):
+		percent = float(x) / fileCount
+		hashes = '#' * int(round(percent * 20))
+		spaces = ' ' * (20 - len(hashes))
 		randIndex = randint(0, len(srcPathList)-1)
-		copyMp3(srcPathList.pop(randIndex), dstPath)
+		sys.stdout.write("\rProgress: [{0}] {1}%".format(hashes + spaces, int(round(percent * 100))) + " ETA: " + calcEtaStr(startTime, x, fileCount))
+		sys.stdout.flush()
+		copyOrConvert(srcPathList.pop(randIndex), dstPath)
+
+	sys.stdout.write("\rProgress: [{0}] {1}%".format(hashes + spaces, 100) + " ETA: " + calcEtaStr(startTime, x, fileCount))
+	sys.stdout.flush()
+
+def calcEtaStr(startTime, position, total):
+	elapsedTime = datetime.datetime.now() - startTime
+	estimatedRemaining = elapsedTime * total / (position+1)
+	estimatedEndTime = datetime.datetime.now() + estimatedRemaining
+	estimatedTimeToComplete = estimatedEndTime - datetime.datetime.now()
+	return estimatedTimeToComplete.__str__()
+
+def checkFFmpeg():
+	try:
+		f = open(os.devnull, "w")
+		subprocess.check_call(['ffmpeg', '-h'], stdout=f, stderr=subprocess.STDOUT)
+	except subprocess.CalledProcessError:
+		print("Problem executing ffmpeg, please check your system")
+		sys.exit(1)
+	except OSError:
+		print("Please install ffmepg first: https://www.ffmpeg.org/download.html")
+		sys.exit(1)
+
+def printHelp():
+	f = open('README.md', 'r')
+	print f.read()
+	f.close()
+
+def printUsage():
+	print("Usage: randcopy.py [-h] srcDir dstDir numFiles")
 
 
 def main(argv):
 	if len(argv) != 4:
-		print("Usage: randcopy.py srcDir dstDir numFiles")
-		sys.exit(1)
-
+		if len(argv) == 2 and argv[1] == '-h':
+			printHelp()
+			sys.exit(0)
+		else:
+			printUsage()
+			sys.exit(1)
+	checkFFmpeg()
 	filesSourcePath = argv[1]
 	filesDestinationPath = argv[2]
 	howManyFilesToCopy = int(argv[3])
 
 	print("Source: " + str(filesSourcePath))
 	print("Destination: " + str(filesDestinationPath))
-	print("Count: " + str(howManyFilesToCopy))
+	print("Number of files to copy: " + str(howManyFilesToCopy))
 
 	print("Building src file list...")
 	filesSourcePathList = populateFileList(filesSourcePath, supportedExtensions)
 	print("Src file list complete; number of files = " + str(len(filesSourcePathList)))
-	print("Start copy to dest...")
 	randomCopy(filesSourcePathList, filesDestinationPath, howManyFilesToCopy)
 	print("\nCopy complete")
 
